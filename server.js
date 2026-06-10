0c653630cb02cc2af70643cd29afe0f852a9f5b5
Yahya-Ahmed-09 <notyouryahya123@gmail.com>
2026-06-10 21:30:14 +0500

    Login Register Created

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const connectDB = require("./config/db.js");
+const PORT = process.env.PORT || 3000;
+const authRoutes = require("./routes/authroutes.js")
+
+
+const app = express();
+
+app.use(express.json());
+
+
+connectDB();
+
+app.get("/api", (req, res)=>{
+    res.send("API Is Running")
+})
+app.use("/api/auth", authRoutes);
+app.listen(PORT, ()=>{
+    console.log(`Server Runnig on ${PORT}`)
+})
